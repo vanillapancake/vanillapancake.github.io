d5098b85cbdfffaccd63c94e8fe4d792e2613b93
vanillapancake <78888157+vanillapancake@users.noreply.github.com>
2022-10-25 17:24:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const Username = document.getElementById('Username')
+const Password = document.getElementById('Password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+
+function myFunction() {
+    window.location.href="about.html";
+  }
